d8a5cb2cd09953e0b0a1efe615b893e995ecc0eb
Ellen Bowman <ellenbowman@Ellens-MacBook-Pro.local>
2015-03-11 12:21:30 -0400

    adding polls app

---
+++ content_satellite/mysite/polls/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Question(models.Model):
+	question_text = models.CharField(max_length=200)
+
+class Choice(models.Model):
+	question = models.ForeignKey(Question)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
+	max_votes = models.IntegerField(default=15)
