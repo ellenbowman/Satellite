4d90b4effeece5266394f0fd242dc4eced1b4cc8
Ellen Bowman <ellenbowman@Ellens-MacBook-Pro.local>
2015-03-11 22:24:36 -0400

    modifications to admin.py

--- content_satellite/mysite/satellite/admin.py
+++ content_satellite/mysite/satellite/admin.py
@@ -15,7 +15,7 @@ admin.site.register(Scorecard, ScorecardAdmin)
 
 class ServiceTakeAdmin(admin.ModelAdmin):
 	list_display = ['ticker', 'open_date', 'scorecard', 'action', 'is_core', 'is_newest', 'is_first']
-	list_filter = ['scorecard','is_core', 'is_first', 'is_newest']
+	list_filter = ['scorecard', 'scorecard__service','is_core', 'is_first', 'is_newest']
 	search_fields = ['ticker__ticker_symbol',]
 
 admin.site.register(ServiceTake, ServiceTakeAdmin)
