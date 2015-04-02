8372cf5897ce11f7ede1af325b8d672c68646c0a
Lisa Chung <lchung@fool.com>
2015-04-01 20:30:31 -0400

    refine import_articles.py. we don't want usmf articles, so let's specify that we don't want usmf articles at the start, when we request data from the hydra api (in this case, instead of doing a blacklist of usmf, we'll do a whitelist of the premium services).
    also, rename variable 'jr' to 'article_json', so that it's a little more descriptive.
    
    also, to better approximate 15 articles per ticker, run a counter in the for loop, and increment the counter only after we've called save() on the article. why this instead of something like json_response[:15] in the "for" loop (~line 37)? consider the case where we're in the for loop with ticker FB, and json_response[:15] contains 4 duplicates; in that case, we'd add only 11 articles for FB.

--- content_satellite/satellite/import_articles.py
+++ content_satellite/satellite/import_articles.py
@@ -11,8 +11,7 @@ from models import Article, Ticker, Service
 
 script_start_time = datetime.datetime.now()
 
-BASE_HYDRA_URL = 'https://hydra.fool.com/api/secure/content/query/?stop=7&format=json&instrument_ids='
-
+BASE_HYDRA_URL = 'https://hydra.fool.com/api/secure/content/query/?stop=20&format=json&service_ids=1081,1069,1502,1451,1371,1321,1255,1228,1128,1066,1062,1048,1008&instrument_ids='
 
 for ticker in Ticker.objects.all().order_by('ticker_symbol'):
 	
@@ -33,18 +32,24 @@ for ticker in Ticker.objects.all().order_by('ticker_symbol'):
 		# let's not bother trying to process the articles
 		continue
 
-	# let's get the first 10 items in the list. 
-	for jr in json_response[:10]:
+	# let's get the first 15 items in the list. 
+	count_of_articles_for_this_ticker = 0
+	for article_json in json_response:
+
+		if count_of_articles_for_this_ticker >=15:
+			break
+
 		article = Article()
-		article.title = jr['headline'][:100]
+		article.title = article_json['headline'][:100]
 		
 		# examples of values we expect in ['service']['slug']: 'hidden_gems','stock_advisor','supernova'
 		# we need to have corresponding Service records
-		service_slug = jr['service']['slug']
+		service_slug = article_json['service']['slug']
 		# find those corresponding Service records. in practice, we should get at most 1 match
 		services = Service.objects.filter(name=service_slug)
 		if len(services)==0:
 			# if we don't find a match, then assume it's a usmf article
+			print 'no match for service', service_slug
 			continue
 		else:
 			service = services[0]
@@ -53,25 +58,27 @@ for ticker in Ticker.objects.all().order_by('ticker_symbol'):
 		# assign the url. in the json, the "base domain" of the url is not defined - eg only the part of the path after 'newsletters.fool.com' and 'www.fool.com'. 
 		# so based on the service, we decide what base domain to use. this is
 		# so that the user can copy-paste a complete url
-		if jr['legacy_uri']:
+		if article_json['legacy_uri']:
 			content_base_url = 'newsletters.fool.com'
 			if service_slug == 'usmf_free':
 				continue
-			article.url = content_base_url + jr['legacy_uri']
+			article.url = content_base_url + article_json['legacy_uri']
 			url_matches = Article.objects.filter(url=article.url, ticker=ticker)
 			if len(url_matches) >0:
-				print 'found duplicate' + article.url
+				print 'found duplicate', article.url
 				continue
 
 
-		article.author = jr['byline'][:50]
-		publish_date = jr['publish_at']
+		article.author = article_json['byline'][:50]
+		publish_date = article_json['publish_at']
 		publish_date = publish_date.split('T')[0]
 		article.date_pub = datetime.datetime.strptime(publish_date, '%Y-%m-%d')
 
 		article.ticker  = ticker
 		
 		article.save()
+
+		count_of_articles_for_this_ticker += 1
 
 script_end_time = datetime.datetime.now()
 total_seconds = (script_end_time - script_start_time).total_seconds()
