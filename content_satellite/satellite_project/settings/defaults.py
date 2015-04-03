3905ff17ee2bbd00d379c6e45c9aba5c3984c05f
Ellen Bowman <ellenbowman@MacBook-Pro.local>
2015-04-03 13:18:53 -0400

    Removing alteration of database in defaults.py

--- content_satellite/satellite_project/settings/defaults.py
+++ content_satellite/satellite_project/settings/defaults.py
@@ -74,19 +74,6 @@ DATABASES = {
 }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'apimanager',
-        'USER': 'apimanager_admin',
-        'PASSWORD': 'eW76BUtVR&zxryXk!4BhTKM^qP9t#wX9D%yqvxW4Nqgw2Z%RbbqBuC66Zf&M5DW@',
-        'HOST': 'lntedjangodb01.foolhq.com',
-        'PORT': '5432',
-        }
-}
-
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
