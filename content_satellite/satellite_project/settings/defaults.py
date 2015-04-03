fef72ca0af1a7242ed6d46835618b91dcdb0685f
Lisa Chung <lchung@fool.com>
2015-04-03 12:41:11 -0400

    default to the postgres test-enviro db (no sqlite3)

--- content_satellite/satellite_project/settings/defaults.py
+++ content_satellite/satellite_project/settings/defaults.py
@@ -74,6 +74,17 @@ DATABASES = {
 }
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'apimanager',
+        'USER': 'apimanager_admin',
+        'PASSWORD': 'eW76BUtVR&zxryXk!4BhTKM^qP9t#wX9D%yqvxW4Nqgw2Z%RbbqBuC66Zf&M5DW@',
+        'HOST': 'lntedjangodb01.foolhq.com',
+        'PORT': '5432',
+        }
+}
+
 
 
 # Internationalization
