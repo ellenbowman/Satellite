b44dc99badc13e651e37bb3cce5505e38d6bf2be
Ellen Bowman <ellen.simonson@gmail.com>
2015-04-09 21:14:57 -0400

    changed title of page to Satellite in admin in views.py

--- content_satellite/satellite/views.py
+++ content_satellite/satellite/views.py
@@ -3,11 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+	context = {
+		'page-title': 'Satellite'
+	}
 	return HttpResponse("""
 		<div align='center'; style='font-family:Verdana, Arial, Helvetica, sans-serif'>
 		<p><a href='/admin/satellite/ticker/'><img width='700px' src='http://g.foolcdn.com/editorial/images/150992/welcome_large.png'/></a></p>
 		</div>
-		""")
+		""", context)
 
 def editors(request):
 	context = {
