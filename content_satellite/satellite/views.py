ff08bc384778abc0624cd20b9bc9f2c292a87a17
Lisa Chung <lchung@fool.com>
2015-04-28 04:29:48 -0400

    update view info_by_scorecard so that it processes POST data

--- content_satellite/satellite/views.py
+++ content_satellite/satellite/views.py
@@ -71,13 +71,47 @@ def articles_by_service(request):
 def info_by_scorecard(request):
 	
 	scorecard_name = None
-	    # let's see if the user passed along a 'scorecard' in the query string
+	# let's see if the user passed along a 'scorecard' in the query string
 	if 'scorecard' in request.GET:
 		scorecard_name = request.GET['scorecard']
 		# i see a match! let's store it in a local variable
 		# (ie exists only for the life of this function) called 'scorecard_name'
 		# eb: below I made it a global variable, I believe, so I could put it
 		# into the html view
+
+
+	# and now, let's see if there's anything interesting in the PUT dictionary
+	# one use case of the PUT dictionary: from form actions, where data is passed 
+	# along in the request, just not as a query string
+	if request.POST:
+		
+		# find the keys that correspond to the 'notes' input. 
+		# since we also have control over the forms markup (embedded in 'info_by_scorecard.html'), 
+		# we know that the form data we're interested in has names that start with 'ticker_notes_'.
+		# it translates that these names should be visible as keys in the request.POST dictionary
+
+		ticker_note_name_prefix = 'ticker_notes_'
+
+		# use 'python list comprehension' to create a list of all the keys in request.POST that 
+		# match this condition: the key must start with 'ticker_notes_' . equivalent to a multi-line
+		# 'for' loop.
+		keys_of_ticker_note_data = [key_in_post_dict for key_in_post_dict in request.POST.keys() if key_in_post_dict.startswith(ticker_note_name_prefix)]
+		
+		for key_of_ticker_note_data in keys_of_ticker_note_data:
+			# from each key, we can extract the Ticker id that we've embedded in the key
+			# (eg, if we see 'ticker_notes_3', we know it corresponds to the Ticker with id 3)
+			# and we can use that id to retrieve the Ticker object from the db,
+			# update its notes field, and save the Ticker. voila!
+
+			ticker_id = key_of_ticker_note_data[len(ticker_note_name_prefix):]  # pick out everything in the string that follows the 'ticker_notes_' prefix
+			ticker_to_update = Ticker.objects.get(id=ticker_id)
+
+			ticker_to_update.notes = request.POST[key_of_ticker_note_data] # retrieve from the POST dictionary the user input corresponding to this Ticker object
+			ticker_to_update.save() # write this update to the db!
+			
+			# print to console a sanity check
+			print 'updated Ticker %s (id: %s). notes value: %s' % (ticker_to_update.ticker_symbol, ticker_id, ticker_to_update.notes)  
+
 
 	max_count = 10
 
