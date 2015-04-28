7ee028dbcf6478a42b617db45c703a0ea2d7a6df
Ellen Bowman <ebowman@val-ebowman.local>
2015-04-28 12:21:17 -0400

    Redesigning info_by_scorecard to be more like articles_vomit. Want a list of biggest movers in descending order, filter-able by service

--- content_satellite/satellite/views.py
+++ content_satellite/satellite/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from models import Ticker, Service, Scorecard, ServiceTake, Article
 from django.http import HttpResponseRedirect
-from forms import NotesForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -72,6 +72,142 @@ def articles_by_service(request):
 ###############################################################################
 
 def info_by_scorecard(request):
+# copied from grand_vision_articles
+	"""
+	shows all tickers and some meta data
+	(daily percent change, company name, exchange, ticker symbol)
+
+	contains a form that lets you specify services
+
+	if a service name is detected in the request's POST dictionary, then filters to tickers for that service
+	"""
+
+	services_to_filter_by = None 	# will hold the Service objects that satisfy our filter
+	service_filter_description = None   # this will be a string description of the service filter. we'll display this value on the page.
+
+
+	#---- start of handling a service filter submitted via POST request ---------
+
+		# figure out what services were selected in the form. 
+		# we've coded the html so that the input checkboxes are named 'filter_service_x', where
+		# x is the id of a service, and the value is also the id of a service
+		# in this step, we find out which of those 'filter_service_x' have been passed back in the POST
+	service_filter_keys = [k for k in request.POST.keys() if k.startswith('filter_service_')]
+	if len(service_filter_keys):
+
+		services_to_filter_by = []
+		for key in service_filter_keys:
+			service_id = request.POST[key]  # we know the value will be a service id, b/c that's how we coded the html!
+			service_match_for_this_id = Service.objects.get(id=service_id) 
+			services_to_filter_by.append(service_match_for_this_id)
+
+		# make the pretty description of the services we found. 
+		pretty_names_of_services_we_matched = [s.pretty_name for s in services_to_filter_by]
+		pretty_names_of_services_we_matched.sort()
+		service_filter_description = ', '.join(pretty_names_of_services_we_matched)
+	else:	
+		# user didn't specify any services. that's fine. our services_to_filter_by, earlier set to None, is untouched.
+		pass
+	#---- end of handling a service filter submitted via POST request ---------
+
+
+
+	# get the set of tickers, filtered by service, if those filters are defined
+	if services_to_filter_by:
+		tickers = Ticker.objects.filter(servicetake=services_to_filter_by) #.order_by('-date_pub')		
+	else:
+		# get all tickers, and sort by biggest mover
+		tickers = Ticker.objects.all().order_by('-daily_percent_change')
+
+	# introduce django's built-in pagination!! let each page show 50 articles
+	# https://docs.djangoproject.com/en/1.7/topics/pagination/
+	paginator = Paginator(tickers, 50) 
+
+	# let's see if the query string already has a value for which 
+	# page we should show (eg: '/sol/articles_vomit/?page=4')
+	# this could be introduced by the user or by a link on our page
+	page_num = request.GET.get('page')
+
+	try:
+		tickers_subset = paginator.page(page_num)
+	except PageNotAnInteger:
+		# page is not an integer; let's show the first page of results
+		tickers_subset = paginator.page(1)
+	except EmptyPage:
+		# the user asked for a page way beyond what we have available;
+		# let's show the last page of articles, which we can calculate
+		# with paginator.num_pages
+		tickers_subset = paginator.page(paginator.num_pages)
+
+
+	# compile meta data -------------------
+	## we already sorted the articles by pub date. to get the newest and oldest, 
+	## we just look at the first element in the list, and the last element
+	# article_most_recent_date = articles[0].date_pub  
+	#article_oldest_date = articles[len(articles)-1].date_pub
+
+	## how many authors?
+	# authors = [art.author for art in articles]
+	# '''  the above line is equivalent to the bottom 3! an example of "list comprehension"
+	# authors = []
+	# for art in articles:
+	#	authors.append(art.author)
+	# '''
+	## convert into a set, so that we toss out duplicates
+	# authors_set = set(authors)
+	# num_authors = len(authors_set)
+
+	### how many articles?
+	# num_articles = len(articles)
+
+	service_options = Service.objects.all().order_by('pretty_name')
+
+	# and now, let's see if there's anything interesting in the PUT dictionary
+	# one use case of the PUT dictionary: from form actions, where data is passed 
+	# along in the request, just not as a query string
+	if request.POST:
+		
+		# find the keys that correspond to the 'notes' input. 
+		# since we also have control over the forms markup (embedded in 'info_by_scorecard.html'), 
+		# we know that the form data we're interested in has names that start with 'ticker_notes_'.
+		# it translates that these names should be visible as keys in the request.POST dictionary
+
+		ticker_note_name_prefix = 'ticker_notes_'
+
+		# use 'python list comprehension' to create a list of all the keys in request.POST that 
+		# match this condition: the key must start with 'ticker_notes_' . equivalent to a multi-line
+		# 'for' loop.
+		keys_of_ticker_note_data = [key_in_post_dict for key_in_post_dict in request.POST.keys() if key_in_post_dict.startswith(ticker_note_name_prefix)]
+		
+		for key_of_ticker_note_data in keys_of_ticker_note_data:
+			# from each key, we can extract the Ticker id that we've embedded in the key
+			# (eg, if we see 'ticker_notes_3', we know it corresponds to the Ticker with id 3)
+			# and we can use that id to retrieve the Ticker object from the db,
+			# update its notes field, and save the Ticker. voila!
+
+			ticker_id = key_of_ticker_note_data[len(ticker_note_name_prefix):]  # pick out everything in the string that follows the 'ticker_notes_' prefix
+			ticker_to_update = Ticker.objects.get(id=ticker_id)
+
+			ticker_to_update.notes = request.POST[key_of_ticker_note_data] # retrieve from the POST dictionary the user input corresponding to this Ticker object
+			ticker_to_update.save() # write this update to the db!
+			
+			# print to console a sanity check
+			print 'updated Ticker %s (id: %s). notes value: %s' % (ticker_to_update.ticker_symbol, ticker_id, ticker_to_update.notes)
+
+	dictionary_of_values = {
+		'tickers': tickers_subset,
+		#'pub_date_newest': article_most_recent_date,
+		#'pub_date_oldest': article_oldest_date,
+		#'num_authors' : num_authors,
+		# 'num_tickers' : num_tickers,
+		'service_options' : service_options, 
+		'service_filter_description': service_filter_description,
+		#'ticker_filter_description': ticker_filter_description
+	}
+	return render(request, 'satellite/info_by_scorecard.html', dictionary_of_values)
+
+"""
+def info_by_scorecard(request):
 	
 	scorecard_name = None
 	# let's see if the user passed along a 'scorecard' in the query string
@@ -79,9 +215,6 @@ def info_by_scorecard(request):
 		scorecard_name = request.GET['scorecard']
 		# i see a match! let's store it in a local variable
 		# (ie exists only for the life of this function) called 'scorecard_name'
-		# eb: below I made it a global variable, I believe, so I could put it
-		# into the html view
-
 
 	# and now, let's see if there's anything interesting in the PUT dictionary
 	# one use case of the PUT dictionary: from form actions, where data is passed 
@@ -126,7 +259,7 @@ def info_by_scorecard(request):
 		scorecard_take = ServiceTake.objects.filter(scorecard__in=scorecard_match) #[:max_count]
 
 	else:
-		scorecard_take = ServiceTake.objects.all() #[:max_count]	
+		scorecard_take = ServiceTake.objects.all()[:max_count]	
 
 		# so now you have ServiceTake objects that are associated with the Scorecard objects
 		# that have name 'Pro' -- below it prints the number of 'em in the terminal
@@ -153,17 +286,25 @@ def info_by_scorecard(request):
 	print 'ticker matches:', len(ticker_matches_list), '------------------'
 	ticker_matches_list.sort(key=lambda x: x.daily_percent_change, reverse=True)
 
+
+	# let's try to make the biggest movers list filter-able by service
+	service_options = Service.objects.all().order_by('pretty_name')
+
+
 	dictionary_of_values = {
 		'scorecard_name' : scorecard_name,
 		'scorecard_take' : scorecard_take,
 		'ticker_matches_list' : ticker_matches_list,
+		'service_options' : service_options,
 		}
 	
 
-	return render(request, 'satellite/info_by_scorecard.html', dictionary_of_values)	
+	return render(request, 'satellite/info_by_scorecard.html', dictionary_of_values)
+
+"""
 
 
-#############################################################################
+###########################################################################
  
 def edit_notes_page(request):
 	# if this is a POST request we need to process the form data
