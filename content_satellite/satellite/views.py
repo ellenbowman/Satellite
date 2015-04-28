3e6a2db77359c42769a0be445dae02bd379c5283
Ellen Bowman <ebowman@val-ebowman.local>
2015-04-28 15:30:59 -0400

    changing name of info by scorecard to movers by service, to better reflect what it does. deleting some experimental views i no longer need

--- content_satellite/satellite/views.py
+++ content_satellite/satellite/views.py
@@ -71,7 +71,7 @@ def articles_by_service(request):
 	
 ###############################################################################
 
-def info_by_scorecard(request):
+def movers_by_service(request):
 # copied from grand_vision_articles
 	"""
 	shows all tickers and some meta data
@@ -211,125 +211,7 @@ def info_by_scorecard(request):
 		'service_filter_description': service_filter_description,
 		#'ticker_filter_description': ticker_filter_description
 	}
-	return render(request, 'satellite/info_by_scorecard.html', dictionary_of_values)
-
-"""
-def info_by_scorecard(request):
-	
-	scorecard_name = None
-	# let's see if the user passed along a 'scorecard' in the query string
-	if 'scorecard' in request.GET:
-		scorecard_name = request.GET['scorecard']
-		# i see a match! let's store it in a local variable
-		# (ie exists only for the life of this function) called 'scorecard_name'
-
-	# and now, let's see if there's anything interesting in the PUT dictionary
-	# one use case of the PUT dictionary: from form actions, where data is passed 
-	# along in the request, just not as a query string
-	if request.POST:
-		
-		# find the keys that correspond to the 'notes' input. 
-		# since we also have control over the forms markup (embedded in 'info_by_scorecard.html'), 
-		# we know that the form data we're interested in has names that start with 'ticker_notes_'.
-		# it translates that these names should be visible as keys in the request.POST dictionary
-
-		ticker_note_name_prefix = 'ticker_notes_'
-
-		# use 'python list comprehension' to create a list of all the keys in request.POST that 
-		# match this condition: the key must start with 'ticker_notes_' . equivalent to a multi-line
-		# 'for' loop.
-		keys_of_ticker_note_data = [key_in_post_dict for key_in_post_dict in request.POST.keys() if key_in_post_dict.startswith(ticker_note_name_prefix)]
-		
-		for key_of_ticker_note_data in keys_of_ticker_note_data:
-			# from each key, we can extract the Ticker id that we've embedded in the key
-			# (eg, if we see 'ticker_notes_3', we know it corresponds to the Ticker with id 3)
-			# and we can use that id to retrieve the Ticker object from the db,
-			# update its notes field, and save the Ticker. voila!
-
-			ticker_id = key_of_ticker_note_data[len(ticker_note_name_prefix):]  # pick out everything in the string that follows the 'ticker_notes_' prefix
-			ticker_to_update = Ticker.objects.get(id=ticker_id)
-
-			ticker_to_update.notes = request.POST[key_of_ticker_note_data] # retrieve from the POST dictionary the user input corresponding to this Ticker object
-			ticker_to_update.save() # write this update to the db!
-			
-			# print to console a sanity check
-			print 'updated Ticker %s (id: %s). notes value: %s' % (ticker_to_update.ticker_symbol, ticker_id, ticker_to_update.notes)  
-
-
-	max_count = 10
-
-# so the below is showing us all the tickers in the Pro scorecard.
-
-	if scorecard_name:
-		scorecard_match = Scorecard.objects.filter(pretty_name=scorecard_name)
-		print 'any matches?', len(scorecard_match), scorecard_match, '!!!!!!!!!!!!!!!!!'
-		scorecard_take = ServiceTake.objects.filter(scorecard__in=scorecard_match) #[:max_count]
-
-	else:
-		scorecard_take = ServiceTake.objects.all()[:max_count]	
-
-		# so now you have ServiceTake objects that are associated with the Scorecard objects
-		# that have name 'Pro' -- below it prints the number of 'em in the terminal
-
-	print 'how many scorecard_take ?', len(scorecard_take), '!!!!!!!!!!!!'
-
-
-# now, to get the ticker symbols associated with ServiceTake objects?
-# how about a "for" loop that collects the value of the 'ticker' field from each ServiceTake?
-
-
-	ticker_matches = set()
-	for st in scorecard_take:
-		ticker_matches.add(st.ticker)
-		# print 'what is the ticker?', st, '!!!!!!!!'
-
-	# that's all we need. but let's refine things. let's alphabetize the tickers.
-    # while a 'set' is convenient because we'll avoid duplicates,
-    # sets don't have a sense of ordering. lists have a sense of ordering.
-    # let's convert the set to a list and sort it.
-
-
-	ticker_matches_list = list(ticker_matches)
-	print 'ticker matches:', len(ticker_matches_list), '------------------'
-	ticker_matches_list.sort(key=lambda x: x.daily_percent_change, reverse=True)
-
-
-	# let's try to make the biggest movers list filter-able by service
-	service_options = Service.objects.all().order_by('pretty_name')
-
-
-	dictionary_of_values = {
-		'scorecard_name' : scorecard_name,
-		'scorecard_take' : scorecard_take,
-		'ticker_matches_list' : ticker_matches_list,
-		'service_options' : service_options,
-		}
-	
-
-	return render(request, 'satellite/info_by_scorecard.html', dictionary_of_values)
-
-"""
-
-
-###########################################################################
- 
-def edit_notes_page(request):
-	# if this is a POST request we need to process the form data
-    if request.method == 'POST':
-    # create a form instance and populate it with data from the request:
-       form = NotesForm(request.POST)
-       # check whether it's valid:
-       if form.is_valid():
-       		# process the data in form.cleaned_data as required
-       		# ...
-       		# ... redirect to a new URL:
-            return HttpResponseRedirect('/sol/edit_notes_page/')
-        	# if a GET (or any other method) we'll create a blank form
-    else:
-    	form = NotesForm()
-
-    return render(request, 'satellite/edit_notes_page.html', { 'form': form}) #sol/edit_notes_page
-
+	return render(request, 'satellite/movers_by_service.html', dictionary_of_values)
 
 ###############################################################################
 
