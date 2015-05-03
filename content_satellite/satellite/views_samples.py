ellenbowman/Satellite

commit ba70b29cefce5ad45577cdc0596848d5cbd6ab7b

articles vomit: fix bug where filter was lost if user tried to view other pages of the filtered set. key: change the page navigation to a POST in which we secretly pass along the form again. along the way, refactor some logic into their own functions.  also along the way, build in support for tickers/service filter being passed along in request.GET, eg "/the_articles/?tickers=AAPL,DDD&service_ids=3"

content_satellite/satellite/views_samples.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.urlresolvers import reverse
 from django.shortcuts import render
 from django.http import HttpResponse
 from forms import ArticlesFilterForm
@@ -87,7 +88,33 @@ def extra_views_homepage(request):
 	return render(request, 'satellite/extra_pages.html')
 
 
+def _get_ticker_objects_for_ticker_symbols(ticker_symbols_csv='AAPL,SWIR,Z'):
+	"""
+	given a set of ticker symbols as a single string, symbols separated by commas, find corresponding Ticker objects
+	"""
+	csv_elements = ticker_symbols_csv.split(',')
+
+	# clean up - for each element, strip whitespace and convert to uppercase
+	csv_elements = [el.strip().upper() for el in csv_elements]
+
+	return Ticker.objects.filter(ticker_symbol__in=csv_elements)
+
+def _get_service_objects_for_service_ids(service_ids_csv='1,4,7'):
+	"""
+	given a set of db ids of services, find corresponding Service objects
+	# note: these ids were assigned by our db. they are *not* the service's product ids (eg Rule Breakers might have
+		a product id of 1069, but in our db, its db id might be 3)
+	"""	
+	csv_elements = service_ids_csv.split(',')
+
+	# clean up - for each element, strip whitespace and convert to an integer
+	csv_elements = [int(el.strip()) for el in csv_elements]
+
+	return Service.objects.filter(id__in=csv_elements)
+
+
 def grand_vision_articles(request):
+
 	"""
 	shows all articles and some meta data
 	(time range of the articles, unique authors)
@@ -103,32 +130,24 @@ def grand_vision_articles(request):
  	ticker_filter_description = None 	# this will be a string description of the ticker filter. we'll display this value on the page.
 	service_filter_description = None   # this will be a string description of the service filter. we'll display this value on the page.
 
+	page_num = 1
 
-	#---- start of handling a ticker/service filter submitted via POST request ---------
+	# filter by ticker/service if we detect that preference in the query string (in the request.GET)
+	# or via a form post (in the request.POST)
+	# additionally, if this is a GET, let's attempt to set the page_num. otherwise, we'll default to page_num of 1.
 
 	if request.POST:
+		if 'page_number' in request.POST:
+			page_num = int(request.POST['page_number'])
+
 		article_filter_form = ArticlesFilterForm(request.POST)
 		
 		if article_filter_form.is_valid():
 			if 'tickers' in article_filter_form.cleaned_data:
-
 				tickers_user_input = article_filter_form.cleaned_data['tickers'].strip()
 				if tickers_user_input != '':
-					user_tickers_as_list = tickers_user_input.split(",")
-
 					# take the user input and try to find corresponding Ticker objects 
-					# the list of matches across all of the tickers in user_tickers_as_list
-					tickers_to_filter_by = [] 
-					for ticker_symbol in user_tickers_as_list:
-						cleaned_up_ticker_symbol = ticker_symbol.strip()
-						ticker_matches_for_this_single_ticker_symbol = Ticker.objects.filter(ticker_symbol__iexact=cleaned_up_ticker_symbol)
-
-						# add the list of matches on this particular ticker symbol to tickers_to_filter_by
-						# http://www.tutorialspoint.com/python/list_extend.htm
-						tickers_to_filter_by.extend(ticker_matches_for_this_single_ticker_symbol)
-
-					# make the pretty description of the tickers
-					ticker_filter_description = tickers_user_input.upper()
+					tickers_to_filter_by = _get_ticker_objects_for_ticker_symbols(tickers_user_input)
 
 			# retrieve the services that were selected in the form. 
 			if 'services' in article_filter_form.cleaned_data:
@@ -137,14 +156,33 @@ def grand_vision_articles(request):
 				# to what the user selected.
 				services_to_filter_by = article_filter_form.cleaned_data['services']
 
-				# make the pretty description of the services we found. 
-				pretty_names_of_services_we_matched = [s.pretty_name for s in services_to_filter_by]
-				pretty_names_of_services_we_matched.sort()
-				service_filter_description = ', '.join(pretty_names_of_services_we_matched)
-		
-	#---- end of handling a ticker/service filter submitted via POST request ---------
+	elif request.GET:
+		initial_form_values = {}
+
+		if 'tickers' in request.GET:
+			tickers_user_input = request.GET.get('tickers')
+			tickers_to_filter_by = _get_ticker_objects_for_ticker_symbols(tickers_user_input)
+
+			initial_form_values['tickers'] = tickers_user_input
+		if 'service_ids' in request.GET:
+			services_to_filter_by = _get_service_objects_for_service_ids(request.GET.get('service_ids'))
+			initial_form_values['services'] = services_to_filter_by
+
+		article_filter_form = ArticlesFilterForm(initial=initial_form_values)
+
 	else:
 		article_filter_form = ArticlesFilterForm()
+
+	# end of inspecting request.GET and request.POST for ticker/service filter
+
+	if tickers_to_filter_by:
+		# make the pretty description of the tickers
+		ticker_filter_description = tickers_user_input.upper()
+	if services_to_filter_by:
+		# make the pretty description of the services we found. 
+		pretty_names_of_services_we_matched = [s.pretty_name for s in services_to_filter_by]
+		pretty_names_of_services_we_matched.sort()
+		service_filter_description = ', '.join(pretty_names_of_services_we_matched)
 
 
 	# get the set of articles, filtered by ticker/service, if those filters are defined
@@ -162,10 +200,6 @@ def grand_vision_articles(request):
 	# https://docs.djangoproject.com/en/1.7/topics/pagination/
 	paginator = Paginator(articles, 25) 
 
-	# let's see if the query string already has a value for which 
-	# page we should show (eg: '/sol/articles_vomit/?page=4')
-	# this could be introduced by the user or by a link on our page
-	page_num = request.GET.get('page')
 
 	try:
 		articles_subset = paginator.page(page_num)
@@ -243,4 +277,5 @@ def grand_vision_articles(request):
 		'service_filter_description': service_filter_description,
 		'ticker_filter_description': ticker_filter_description
 	}
+
 	return render(request, 'satellite/index_of_articles.html', dictionary_of_values)

content_satellite/satellite/templates/satellite/index_of_articles.html
@@ -3,27 +3,39 @@
 	<title>index of articles</title>
 	<style>
 		select.service-select-tall{
-			min-height: 210px;
+			min-height: 140px;
+		}
+
+		.button-link {
+			background-color: transparent;
+			
+			color: blue;
+		}
+
+		.button-link.disabled {
+			background-color: transparent;
+			
+			color: lightgray;
 		}
 	</style>
 </head>
 <body>
-	<h2>Article matches</h2>
+	<h2>Articles</h2>
 	
 	<div>
 		<table>
 			<tr>
 				<td style='vertical-align:top'>
-					how many articles are represented? {{num_articles}}
+					how many articles are in this result set? {{num_articles}}
 					<br/>
 					the most recent article in this result set was published on {{pub_date_newest}}
 					<br/>
 					the oldest article in this results set was published on {{pub_date_oldest}}
 					<br/>
-					how many authors are represented? {{num_authors}}
+					how many authors are in this result set? {{num_authors}}
 
 					{% if service_filter_description or ticker_filter_description %}
-					<div style='background-color:lightgray;width:500px;margin:12px 2px;'>
+					<div style='padding:12px;background-color:lightgray;width:500px;margin:12px 2px;'>
 						These results are filtered.
 						<br/>
 
@@ -42,28 +54,22 @@
 						<br/>
 						<a href='/sol/articles_vomit/'><button type='button'>Remove filter</button></a>
 						<br/>
-
-						*known bug: if the results span more than one page... sorry, but we lose the filter when advancing to subsequent pages. 
 					</div>
 					{% endif %}
 				</td>
 				<td>
-					<div style='padding-left:15px;max-width:180px'>
-						Filter options
+					<div style='padding:15px;max-width:280px;background-color:lightgray'>
 						<form action="" method="post">
 							{% csrf_token %}
-							<hr/>
 							Tickers:<br/>
 							{{form.tickers}}
 							<br/>
 							<br/>
 							Services:<br/>{{form.services}}
-
 							<br/>
 							<hr/>
 							 <input type="submit" value="Apply filter">
 						</form>
-						* we could also filter by authors and date
 					</div>
 				</td>
 			</tr>
@@ -107,9 +113,22 @@
 
 
 	<div class="pagination">
-	    <span class="step-links">
+		<form action="" method="POST">
+			{% csrf_token %}
+			<div style='display:none'>
+				{{form.as_p}}
+			</div>
+			
+
+
+			<span class="step-links">
+
+
+
 	        {% if articles.has_previous %}
-	            <a href="?page={{ articles.previous_page_number }}">previous</a>
+	        	<button type='submit' class="button-link" name='page_number' value="{{articles.previous_page_number}}">&lt;&lt;</button>
+	        {% else %}
+	        	<button type='button' class="button-link disabled" disabled='disabled'> &lt;&lt;</button>
 	        {% endif %}
 
 	        <span class="current">
@@ -117,9 +136,15 @@
 	        </span>
 
 	        {% if articles.has_next %}
-	            <a href="?page={{ articles.next_page_number }}">next</a>
+	            <button type='submit' class="button-link" name='page_number' value="{{articles.next_page_number}}">&gt;&gt;</button>
+	        {% else %}
+	        	<button type='button' class="button-link disabled" disabled='disabled'> &gt;&gt;</button>
 	        {% endif %}
 	    </span>
+
+		</form>
+
+	    
 	</div>
 
 </div>

commit 25c9590e538fd3c3735b1719214a5bc23dd48509

oops. fix bug introduced in ba70b29ce - when you try to view anything beyond the first page of results, the page you'd receive might have 0 results. bug in letting a filter of 0 services causing the contraint to be "make sure the articles you return are associated with one of these zero services"

content_satellite/satellite/views_samples.py
@@ -137,6 +137,7 @@ def grand_vision_articles(request):
 	# additionally, if this is a GET, let's attempt to set the page_num. otherwise, we'll default to page_num of 1.
 
 	if request.POST:
+		print request.POST
 		if 'page_number' in request.POST:
 			page_num = int(request.POST['page_number'])
 
@@ -151,10 +152,11 @@ def grand_vision_articles(request):
 
 			# retrieve the services that were selected in the form. 
 			if 'services' in article_filter_form.cleaned_data:
-				# the form makes available "cleaned data" that's pretty convenient - 
-				# in this case, it returns a list of Service objects that correspond
-				# to what the user selected.
-				services_to_filter_by = article_filter_form.cleaned_data['services']
+				if len(article_filter_form.cleaned_data['services']) > 0:
+					# the form makes available "cleaned data" that's pretty convenient - 
+					# in this case, it returns a list of Service objects that correspond
+					# to what the user selected.
+					services_to_filter_by = article_filter_form.cleaned_data['services']
 
 	elif request.GET:
 		initial_form_values = {}
@@ -261,7 +263,6 @@ def grand_vision_articles(request):
 
 		article_defns.append({
 			'article':article,
-			'num_articles_by_this_author': len(articles_by_this_author),
 			'author_service_associations': services_in_which_this_author_writes, 
 			'num_author_articles_last_ten_dates': len(articles_by_this_author_from_within_last_ten_days)
 			})