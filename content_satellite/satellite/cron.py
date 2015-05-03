7e77cf870f30948aec96fa8db7cf28407e59da60
Lisa Chung <lchung@fool.com>
2015-05-03 16:33:52 -0400

    auto-run tasks seem to be working (!). in this commit, we refine the schedule.

--- content_satellite/satellite/cron.py
+++ content_satellite/satellite/cron.py
@@ -9,9 +9,8 @@ from django.core.management import call_command
 # http://www.cronchecker.net/  
 
 
-# runs at 30 minute intervals, every day, 7 AM to 8 PM
-@kronos.register('0,30 7-20 * * *')
-def update_articles():
+### articles ------------------
+def _update_articles():
 	print 'starting cron task for importing articles'
 	try:
 		call_command('import_articles')
@@ -19,14 +18,44 @@ def update_articles():
 		print str(e)
 	print 'finished cron task for importing articles'	
 
+# on weekdays run at 30 minute intervals, 8 AM to 7:30 PM
+@kronos.register('0,30 8-19 * * 1-5')
+def update_articles_weekdays():
+	_update_articles()
+
+# on weekends run at 2 hr intervals, 8 AM to 8 PM
+@kronos.register('0 8,10,12,14,16,18,20 * * 6-7')
+def update_articles_weekends():
+	_update_articles()
+
+# every day sweep for articles at 11:59 PM
+@kronos.register('59 23 * * *')
+def update_articles_nightly():
+	_update_articles()
+
+### end of updating articles ------------------
 
 
-# every weekday 10:00AM-5PM, at 15 minute intervals
-@kronos.register('0,15,30,45 10-17 * * 1-5')
-def update_daily_percent_change():
-	print 'starting cron task for updating daily percent change'
+
+### ticker performance ----------------
+def _update_daily_percent_change():
 	try:
 		call_command('update_daily_percent_change')
 	except Exception as e:
 		print str(e)
-	print 'finished cron task for updating daily percent change'
+
+# every weekday at 9:00 AM, 9:45 AM, and 9:50 AM
+# 9:00 AM (to zero-out the fields)
+# 9:45 AM (earliest we expect to get values from Yahoo)
+# 9:50 AM (in case the 9:45 run didn't get values)
+@kronos.register('0,45,50 9 * * 1-5')
+def update_daily_percent_change_market_open():
+	_update_daily_percent_change()
+
+
+# every weekday 10:00AM-4:45PM, at 15 minute intervals.
+@kronos.register('0,15,30,45 10-16 * * 1-5')
+def update_daily_percent_change_normal_hours():
+	_update_daily_percent_change()
+
+### end of updating ticker performance ----------------
