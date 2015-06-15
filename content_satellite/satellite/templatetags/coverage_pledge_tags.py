b487ba6b9a6184e31481b2aeb34c8d8b646fa854
Lisa Chung <lchung@fool.com>
2015-06-15 16:19:13 -0400

    content_type template - introduce a template tag so that, given a ticker and coverage type (5&3, earnings preview), we figure out which services have a CoverageType record. use that logic to determine which checkboxes should be selected.
     https://docs.djangoproject.com/en/1.7/howto/custom-template-tags/#assignment-tags

--- content_satellite/satellite/templatetags/coverage_pledge_tags.py
+++ content_satellite/satellite/templatetags/coverage_pledge_tags.py
@@ -7,7 +7,7 @@ register = template.Library()
 def get_service_pledges(ticker, coverage_choice_id):
     """
     Given a ticker and the integer representation of a coverage choice,
-    find all CoverageType records, and from tha set compile a string of the pretty names of the associated services
+    find all CoverageType records, and from that set compile a string of the pretty names of the associated services
     """
     coverage_pledges = CoverageType.objects.filter(ticker=ticker, coverage_type=coverage_choice_id)
 
@@ -16,3 +16,13 @@ def get_service_pledges(ticker, coverage_choice_id):
         return ', '.join(services)
 
     return ''
+
+
+@register.assignment_tag
+def get_services_ids(coverage_choice_id, ticker):
+    """
+    Given a ticker and the integer representation of a coverage choice,
+    find all CoverageType records, adn from that set compile the ids of teh associated services
+    """
+    coverage_pledges = CoverageType.objects.filter(ticker=ticker, coverage_type=coverage_choice_id)
+    return [cp.service.id for cp in coverage_pledges]
