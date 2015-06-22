c38f8609704ffa79fc7db8ca18bc053ac486f327
Ellen Bowman <ellen.simonson@gmail.com>
2015-06-21 20:47:27 -0400

    Working on template tags, specifically to gather info on preselected authors.

--- content_satellite/satellite/templatetags/coverage_pledge_tags.py
+++ content_satellite/satellite/templatetags/coverage_pledge_tags.py
@@ -22,7 +22,7 @@ def get_service_pledges(ticker, coverage_choice_id):
 def get_services_ids(coverage_choice_id, ticker):
     """
     Given a ticker and the integer representation of a coverage choice,
-    find all CoverageType records, adn from that set compile the ids of teh associated services
+    find all CoverageType records, and from that set compile the ids of the associated services
     """
     coverage_pledges = CoverageType.objects.filter(ticker=ticker, coverage_type=coverage_choice_id)
     return [cp.service.id for cp in coverage_pledges]
