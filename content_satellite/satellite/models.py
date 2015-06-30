bb82ff8540ef815518ae3ff25e3eb411b34c6cda
Ellen Bowman <ebowman@val-ebowman.hq.fool.net>
2015-06-30 10:28:35 -0400

    Adding more selections to coverage types

--- content_satellite/satellite/models.py
+++ content_satellite/satellite/models.py
@@ -114,10 +114,15 @@ GUIDANCE_CHANGE = 6
 COVERAGE_CHOICES = (
 	(TEN_PERCENT_PROMISE, '10% Promise'),
 	(FIVE_AND_THREE, '5 and 3'),
-	(EARNINGS_PREVIEW, 'Earnings Preview'),
-	(EARNINGS_REVIEW, 'Earnings Review'),
+	(EARNINGS_PREVIEW, 'Team Earnings Preview'),
+	(EARNINGS_REVIEW, 'Team Earnings Review'),
 	(RISK_RATING, 'Risk Rating'),
-	(GUIDANCE_CHANGE, 'Guidance Change')
+	(GUIDANCE_CHANGE, 'Guidance Change'),
+	(FOOL_DOT_COM_PREVIEW, 'Fool.com Earnings Preview'),
+	(FOOL_DOT_COM_REVIEW, 'Fool.com Earnings Review'),
+	(TEN_PERCENT_POTENTIAL, '10% Potential'),
+	(TWO_MINUTE_DRILL, '2-Minute Drill'),
+	(BEST_BUYS_NOW, 'Best Buys Now'),
 	)
 
 class CoverageType(models.Model):
