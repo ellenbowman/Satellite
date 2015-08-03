e0f8dbb9c8323ec9080e9358ac23348ef56130eb
Lisa Chung <lchung@fool.com>
2015-08-03 04:32:56 -0400

    Revert "slim down the articles index so that it uses the datatables.js sort/search"
    Why: with the previous (ie non-datatables) implementation, we limited how many articles were returned by the server to 100. with the new implementation, the server returns *all* articles and tries to render *all* articles, as it's the client side where the pagination occurs. at the moment, the new implementation takes way too long to load. we can revisit, consider limiting the number of articles that the server tries to render.
    
    This reverts commit 2fb5d1eba8cb8d1b7986fcfd1767fa4f91467e7d.

--- content_satellite/satellite/views_2.py
+++ content_satellite/satellite/views_2.py
@@ -71,7 +71,78 @@ def get_author_bylines_index(request):
 
 def articles_index(request):
 
-	articles = Article.objects.all().order_by('-date_pub')
+	tickers_to_filter_by = None
+	services_to_filter_by = None
+	ticker_filter_description = None
+ 	service_filter_description = None
+
+	page_num = 1
+
+	if request.POST:
+		if 'page_number' in request.POST:
+			page_num = int(request.POST['page_number'])
+
+		article_filter_form = FilterForm(request.POST)
+
+		if article_filter_form.is_valid():
+			if 'tickers' in article_filter_form.cleaned_data:
+				tickers_user_input = article_filter_form.cleaned_data['tickers'].strip()
+				if tickers_user_input != '':
+					tickers_to_filter_by = _get_ticker_objects_for_ticker_symbols(tickers_user_input)
+
+			if 'services' in article_filter_form.cleaned_data:
+				if len(article_filter_form.cleaned_data['services']) > 0:
+					services_to_filter_by = article_filter_form.cleaned_data['services']
+
+	elif request.GET:
+		initial_form_values = {}
+
+		if 'tickers' in request.GET:
+			tickers_user_input = request.GET.get('tickers')
+			tickers_to_filter_by = _get_ticker_objects_for_ticker_symbols(tickers_user_input)
+
+			initial_form_values['tickers'] = tickers_user_input
+		if 'service_ids' in request.GET:
+			services_to_filter_by = _get_service_objects_for_service_ids(request.GET.get('service_ids'))
+			initial_form_values['services'] = services_to_filter_by
+
+		article_filter_form = FilterForm(initial=initial_form_values)
+
+	else:
+		article_filter_form = FilterForm()
+
+
+	if tickers_to_filter_by:
+		ticker_filter_description = ', '.join([t.strip() for t in tickers_user_input.split(",")])
+	if services_to_filter_by:
+		pretty_names_of_services_we_matched = [s.pretty_name for s in services_to_filter_by]
+		pretty_names_of_services_we_matched.sort()
+		service_filter_description = ', '.join(pretty_names_of_services_we_matched)
+
+
+	if tickers_to_filter_by is not None and services_to_filter_by is not None:
+		articles = Article.objects.filter(ticker__in=tickers_to_filter_by, service__in=services_to_filter_by).order_by('-date_pub')
+	elif tickers_to_filter_by is not None:
+		articles = Article.objects.filter(ticker__in=tickers_to_filter_by).order_by('-date_pub')
+	elif services_to_filter_by is not None:
+		articles = Article.objects.filter(service__in=services_to_filter_by).order_by('-date_pub')
+	else:
+		articles = Article.objects.all().order_by('-date_pub')
+
+
+	paginator = Paginator(articles, 100)
+
+	try:
+		articles_subset = paginator.page(page_num)
+	except PageNotAnInteger:
+		# page is not an integer; let's show the first page of results
+		articles_subset = paginator.page(1)
+	except EmptyPage:
+		# the user asked for a page way beyond what we have available;
+		# let's show the last page of articles, which we can calculate
+		# with paginator.num_pages
+		articles_subset = paginator.page(paginator.num_pages)
+
 
 	if len(articles):
 		article_most_recent_date = articles[0].date_pub
@@ -80,19 +151,35 @@ def articles_index(request):
 		article_most_recent_date = "n/a"
 		article_oldest_date = "n/a"
 
-	authors_set = set([art.author for art in articles])
+	authors = [art.author for art in articles]
+	authors_set = set(authors)
 	num_authors = len(authors_set)
 
 	num_articles = len(articles)
 
+	article_defns = []
+	for article in articles_subset:
+		byline_meta_data = ''
+		byline_match = BylineMetaData.objects.filter(byline=article.author)
+		if byline_match:
+			byline_meta_data = byline_match[0].services
+		article_defns.append({
+			'article':article,
+			'author_service_associations': byline_meta_data,
+			})
+
 	dictionary_of_values = {
-		'articles': articles,
+		'form': article_filter_form,
+		'articles': articles_subset,
+		'article_defns': article_defns,
 		'pub_date_newest': article_most_recent_date,
 		'pub_date_oldest': article_oldest_date,
 		'num_authors' : num_authors,
-		'num_articles' : num_articles
+		'num_articles' : num_articles,
+		'service_filter_description': service_filter_description,
+		'ticker_filter_description': ticker_filter_description
 	}
-	
+
 	return render(request, 'satellite/articles_index.html', dictionary_of_values)
 
 ###################################################################################
