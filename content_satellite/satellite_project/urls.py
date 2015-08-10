3b13c7cfa25c0872e2cbb57aa08d4dd7a209ae5f
Lisa Chung <lchung@fool.com>
2015-08-10 01:09:38 -0400

    wire up url for push notifications page (satellite.fool.com/alerts/)

--- content_satellite/satellite_project/urls.py
+++ content_satellite/satellite_project/urls.py
@@ -5,7 +5,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', include('satellite.urls')), 
+    url(r'^$', include('satellite.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^sol/', include('satellite.urls'))
+    url(r'^sol/', include('satellite.urls')),
+    url(r'^alerts/', include('push_notifications.urls')),
 )
