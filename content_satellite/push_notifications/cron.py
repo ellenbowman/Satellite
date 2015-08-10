175cc5561c748b53eb61be8e150596cfa5d4d749
Lisa Chung <lchung@fool.com>
2015-08-10 10:25:23 -0400

    push notifications - start at 9:55 instead of 10:05.

--- content_satellite/push_notifications/cron.py
+++ content_satellite/push_notifications/cron.py
@@ -2,10 +2,20 @@ import kronos
 from django.core.management import call_command
 
 
-# send out alerts on weekdays at 10 minute intervals, 10:05 AM to 4:55PM
-@kronos.register('5,15,25,35,45,55 10-16 * * 1-5')
-def assess_alerts():
+# send out alerts on weekdays at 10 minute intervals, 9:55 AM to 4:55PM
+
+def _assess_alerts():
 	try:
 		call_command('assess_rules')
 	except Exception as e:
 		print str(e)
+
+# 9:55 AM
+@kronos.register('55 9 * * 1-5')
+def assess_alerts():
+	_assess_alerts()
+
+# 10:05 AM to 4:55 PM
+@kronos.register('5,15,25,35,45,55 10-16 * * 1-5')
+def assess_alerts():
+	_assess_alerts()
