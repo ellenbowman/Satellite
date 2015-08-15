ff8eb922b0797afce6a440581241dd9c3226efdd
Ellen Bowman <ellen.simonson@gmail.com>
2015-08-14 22:04:21 -0400

    Adding _get_promised_coverage_by_service function, which will likely not survive, but at least explains what on earth I am trying to do at the moment.

--- content_satellite/satellite/views.py
+++ content_satellite/satellite/views.py
@@ -424,6 +424,26 @@ def _get_ticker_objects_for_tier_status(tier_status_csv=['core','first']):
 
 ######################################################################################
 
+def _get_promised_coverage_by_service(ticker_symbol):
+
+	ticker = Ticker.objects.get(ticker_symbol=ticker_symbol)
+	if 'Stock Advisor' in ticker.services_for_ticker:
+		ticker.services_for_ticker='10% Promise'
+		ticker.save()
+	else
+		pass
+
+	"""
+	1. get the ticker 
+	2. examine the services_for_ticker
+	3. if [service] in services_for_ticker, append [general coverage promises] to ticker.promised_coverage 
+	4. within each service: if tier exists, append [further coverage promises] 
+	5. save
+
+	"""
+
+#################################################################################
+
 def ticker_overview(request):
 
 	dictionary_of_values = {
@@ -564,8 +584,6 @@ def coverage_detail(request, ticker_symbol):
 			commentary.add(a)
 		if 'news' in a.tags:
 			news.add(a)
-
-	print request.POST
 
 	dictionary_of_values = {
 		'ticker': ticker,
