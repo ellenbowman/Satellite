341e18bd8fdcefec4479e77aa65a72901726ed99
Lisa Chung <lchung@fool.com>
2015-08-23 19:08:24 -0400

    push notifications - as advised by system warnings, add fields to the Meta class of SubscriberForm. also, validate that the slack handle starts with a '@' or '#'

--- content_satellite/push_notifications/forms.py
+++ content_satellite/push_notifications/forms.py
@@ -5,3 +5,13 @@ from push_notifications.models import NotificationSubscriber
 class SubscriberForm(forms.ModelForm):
 	class Meta:
 		model = NotificationSubscriber
+		fields = ('slack_handle','tickers_csv', 'services',)
+
+	def clean_slack_handle(self):
+		data = self.cleaned_data['slack_handle']
+		if not data.startswith('@') and not data.startswith('#'):
+			raise forms.ValidationError("slack handle should start with '@' or '#'")
+		if ' ' in data:
+			raise forms.ValidationError("slack handle should not contain spaces")
+		if len(data) < 3:
+			raise forms.ValidationError("this doesn't look like a slack handle")
