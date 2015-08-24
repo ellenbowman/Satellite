861df9fbfc51d5ccdf7332159b405c2a84836819
Lisa Chung <lchung@fool.com>
2015-08-24 18:32:14 -0400

    move def tiers() from get.py into new management command import_tiers.py. as part of this, move tiers.csv into satellite/management/data.

--- content_satellite/get.py
+++ content_satellite/get.py
@@ -7,38 +7,6 @@ from django.conf import settings
 import csv
 from collections import defaultdict
 
-def tiers():
-	columns = defaultdict(list) # each value in each column is appended to a list
-
-	with open('tiers.csv') as f:
-		reader = csv.DictReader(f) # read rows into a dictionary format
-		for row in reader: # read a row as {column1: value1, column2: value2,...}
-			for (k,v) in row.items(): # go over each column name and value
-				columns[k].append(v) # append the value into the appropriate list
-                                 # based on column name k
-
-	# get the tickers defined in the 'ticker' column.
-	# 	handle cases where a cell contains multiple tickers (separated by commas), eg "GOOGL, GOOG"
-	# 	remove duplicates (via set), and sort (via list)
-	tier_ones = set([t.strip().upper() for t in columns['ticker'] if t != ''])
-	elements_with_commas = set([t for t in tier_ones if ',' in t])
-	tier_ones = tier_ones - elements_with_commas
-	for e in elements_with_commas:
-		tokens = e.split(',')
-		for t in tokens:
-			tier_ones.add(t.strip())
-	tier_ones = list(tier_ones)
-	tier_ones.sort()
-
-	for ticker_symbol in tier_ones:
-		try:
-			ticker = Ticker.objects.get(ticker_symbol = ticker_symbol)
-			ticker.tier = 1
-			ticker.save()
-		except:
-			print 'ticker %s is not in SOL' % ticker_symbol
-
-
 def del_tiers():
 	for t in Ticker.objects.all():
 		if t.tier == 1:
