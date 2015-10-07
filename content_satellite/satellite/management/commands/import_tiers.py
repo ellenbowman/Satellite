2c75ffbb91364c3a3a02d090e59f2b53f732df0e
Ellen Bowman <ellen.simonson@gmail.com>
2015-10-07 13:22:17 -0400

    import_tiers now adds "1" to a ticker's tier field and the service name in which it is a Tier 1 to the tier_status field. Yay!

--- content_satellite/satellite/management/commands/import_tiers.py
+++ content_satellite/satellite/management/commands/import_tiers.py
@@ -18,10 +18,10 @@ class Command(BaseCommand):
         for t in Ticker.objects.all():
             if t.tier == 1:
                 print t.ticker_symbol, t.tier
-                zero = 0
-                t.tier = zero
+                t.tier = 0
+                t.tier_status = ''
                 t.save()
-                print t.ticker_symbol, t.tier
+                print t.ticker_symbol, t.tier, t.tier_status
             else:
                 continue
         t.tier == 0
@@ -36,10 +36,29 @@ class Command(BaseCommand):
     	with open(TIERS_CSV, 'rU') as f:
     		reader = csv.DictReader(f) # read rows into a dictionary format
     		for row in reader: # read a row as {column1: value1, column2: value2,...}
-    			for (k,v) in row.items(): # go over each column name and value
-    				columns[k].append(v) # append the value into the appropriate list
-                                     # based on column name k
+                    ticker_symbol = str(row['ticker'])
+                    service = str(row['service'])
+                    try:
+                        ticker = Ticker.objects.get(ticker_symbol = ticker_symbol)
+                        ticker.tier = 1
+                        ticker.tier_status += ' %s,' % service
+                        ticker.save()
+                    except:
+                        print '%s is not in SOL' % ticker_symbol
+         
+        """
+        EB work-in-progress note: the above solution does not convert tickers to U/C and remove commas like the old code did.
+        However, I plan to maintain the tiers spreadsheet so that there will only ever be one U/C ticker per field.
+        So I will leave that problem for now.
+        Also I need to find out how to make it so I can concat service names into tier_status with commas between names
+        but no comma on the end.
+                Old code below. 
 
+                 #  print row
+                    for (k,v) in row.items(): # go over each column name and value
+                        # print "%s is %s" % (k, v)
+                        columns[k].append(v) # append the value into the appropriate list
+                                     # based on column name k
     	# get the tickers defined in the 'ticker' column.
     	# 	handle cases where a cell contains multiple tickers (separated by commas), eg "GOOGL, GOOG"
     	# 	remove duplicates (via set), and sort (via list)
@@ -60,6 +79,7 @@ class Command(BaseCommand):
     			ticker.save()
     		except:
     			print '%s is not in SOL' % ticker_symbol
+        """
 
         print 'post-import: number of tickers in SOL with tier 1: %d' % get_count_of_tier_one_tickers()
     	print 'finished script'
