fda340467114c9804a9237ccbe5f471f057cd415
Ellen Bowman <ellen.simonson@gmail.com>
2015-10-13 20:11:43 -0400

    deleted some notes, made error message more accurate

--- content_satellite/satellite/management/commands/import_tiers.py
+++ content_satellite/satellite/management/commands/import_tiers.py
@@ -47,7 +47,7 @@ class Command(BaseCommand):
                         ticker.tier_status += ' %s,' % service
                         ticker.save()
                     except:
-                        print '%s is not in SOL' % ticker_symbol
+                        print '%s tier status cannot be updated' % ticker_symbol
          
         """
         EB work-in-progress note: the above solution does not convert tickers to U/C and remove commas like the old code did.
@@ -55,33 +55,7 @@ class Command(BaseCommand):
         So I will leave that problem for now.
         Also I need to find out how to make it so I can concat service names into tier_status with commas between names
         but no comma on the end.
-                Old code below. 
-
-                 #  print row
-                    for (k,v) in row.items(): # go over each column name and value
-                        # print "%s is %s" % (k, v)
-                        columns[k].append(v) # append the value into the appropriate list
-                                     # based on column name k
-    	# get the tickers defined in the 'ticker' column.
-    	# 	handle cases where a cell contains multiple tickers (separated by commas), eg "GOOGL, GOOG"
-    	# 	remove duplicates (via set), and sort (via list)
-    	tier_ones = set([t.strip().upper() for t in columns['ticker'] if t != ''])
-    	elements_with_commas = set([t for t in tier_ones if ',' in t])
-    	tier_ones = tier_ones - elements_with_commas
-    	for e in elements_with_commas:
-    		tokens = e.split(',')
-    		for t in tokens:
-    			tier_ones.add(t.strip())
-    	tier_ones = list(tier_ones)
-    	tier_ones.sort()
-
-    	for ticker_symbol in tier_ones:
-    		try:
-    			ticker = Ticker.objects.get(ticker_symbol = ticker_symbol)
-    			ticker.tier = 1
-    			ticker.save()
-    		except:
-    			print '%s is not in SOL' % ticker_symbol
+        Two tickers are erroring: ZG and PYPL. 
         """
 
         print 'post-import: number of tickers in SOL with tier 1: %d' % get_count_of_tier_one_tickers()
