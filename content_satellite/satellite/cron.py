1bc426bc90dd3fc78adf4ddaa723e8f32d68f45a
Ellen Bowman <ebowman@val-ebowman.hq.fool.net>
2015-10-30 09:22:49 -0400

    Reverting to Oct 6 version of cron.py for now, to see if everything runs when I do that.

--- content_satellite/satellite/cron.py
+++ content_satellite/satellite/cron.py
@@ -39,25 +39,13 @@ def update_articles_nightly():
 
 # on weekdays run first thing in the morning, 8 AM
 @kronos.register('0 8 * * 1-5')
-def update_earnings_announcement_dates():
+def _update_earnings_announcement_dates():
 	try:
-		call_command('update_earnings_announcement_dates')
+		call_command('_update_earnings_announcement_dates')
 	except Exception as e:
 		print str(e)
 
 ### end of updating announcement dates -----------------------
-
-### import new recs and status ----------------------
-
-# on weekdays run first thing in the morning, 8 AM
-@kronos.register('0 8 * * 1-5')
-def import_tick_take():
-	try:
-		call_command('import_tick_take')
-	except Exception as e:
-		print str(e)
-
-### end of updating new recs and status -----------------------
 
 
 ### import ticker status ------------------
@@ -69,7 +57,7 @@ def _import_tick_take():
 	except Exception as e:
 		print str(e)	
 
-### end of updating ticker status ---------------------
+### end of updating ticker status ----------------------------
 
 ### author meta data ------------------
 # every morning at 12:15 AM (not too long after the nightly 11:59 sweep for articles), let's re-compile the author meta data
@@ -81,8 +69,6 @@ def update_author_meta_data_nightly():
 		print str(e)
 
 ### end of updating author meta data ------------------
-
-
 
 ### ticker performance ----------------
 def _update_daily_percent_change():
